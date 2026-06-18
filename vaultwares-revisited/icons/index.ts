6a54dd37cfdf6dbd1383e401a7074df82c7d807b
Philippe Potvin <philippe.potvin1@gmail.com>
2026-06-18 01:01:24 -0400

    removed old themes assets

--- vaultwares-revisited/icons/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './navigation';
-export * from './actions';
-export * from './monitoring';
-export * from './security';
-export * from './media';
-export * from './data';
-export * from './communication';
