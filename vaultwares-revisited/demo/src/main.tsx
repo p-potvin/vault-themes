390c75dfda304ccd7b7e9e32c4e053f87c541b4f
Philippe Potvin <philippe.potvin1@gmail.com>
2026-06-23 20:25:14 -0400

    feat: initialize demo project with Vite, React, and Tailwind CSS

--- vaultwares-revisited/demo/src/main.tsx
+++ vaultwares-revisited/demo/src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { DemoPage } from '../DemoPage';
-import './index.css';
-import '../../revisited.css';
+/*import './index.css';
+import '../../revisited.css';*/
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
